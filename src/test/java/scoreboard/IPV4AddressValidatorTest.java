7c616b023d072b9255905bf3d82bdbd2224cba48
Trepix <pau.trepat.segura@gmail.com>
2021-01-02 02:24:30 +0100

    Create parameterized tests for host assignable ips
    We detect that all tests we could add for the valid case will not force us to create more production code. So we decided to create a parameterized tests to include these cases in order to protect us from future steps that could break the case that is a valid IP.

--- src/test/java/scoreboard/IPV4AddressValidatorTest.java
+++ src/test/java/scoreboard/IPV4AddressValidatorTest.java
@@ -1,22 +1,23 @@
 package scoreboard;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class IPV4AddressValidatorTest {
 
-    @Test
-    public void should_consider_as_valid_a_host_assignable_ip() {
-        boolean isValid = IPV4AddressValidator.isHostAssignable("1.1.1.1");
-
-        assertThat(isValid, is(true));
-    }
-
-    @Test
-    public void should_consider_as_valid_another_host_assignable_ip() {
-        boolean isValid = IPV4AddressValidator.isHostAssignable("192.168.1.1");
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "1.1.1.1",
+            "192.168.1.1",
+            "10.0.0.1",
+            "127.0.0.1"
+    })
+    public void should_consider_as_valid_a_host_assignable_IP(String ip) {
+        boolean isValid = IPV4AddressValidator.isHostAssignable(ip);
 
         assertThat(isValid, is(true));
     }
